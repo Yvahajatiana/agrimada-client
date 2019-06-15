56fd50afd528ccca8614c3fa26400bfb13d409e4
Stephano Romain <stephano.romain@gmail.com>
2019-06-15 13:24:47 +0300

    update input file

---
+++ src/app/back-office/forms/category-form/category-form.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Category } from '../../models/category';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-category-form',
+  templateUrl: './category-form.component.html',
+  styleUrls: ['./category-form.component.scss']
+})
+export class CategoryFormComponent implements OnInit {
+  @Input() value: Category;
+  @Output() valueChange = new EventEmitter<Category>();
+  @Input() errors: any[];
+  @Output() errorsChange = new EventEmitter<any[]>();
+
+  formGroup: FormGroup;
+  private file: File;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.formGroup = this.fb.group({
+      CategoryID: [this.value ? this.value.CategoryID : 0],
+      CategoryName: [
+        this.value ? this.value.CategoryName : null,
+        Validators.required
+      ],
+      Descriptions: [
+        this.value ? this.value.Descriptions : null,
+        Validators.required
+      ],
+      Picture: [this.value ? this.value.Picture : null, Validators.required]
+    });
+  }
+
+  submit(): void {
+    if (this.formGroup.valid) {
+      this.value = this.formGroup.value;
+      this.value.Picture = this.file;
+      this.valueChange.emit(this.value);
+    }
+  }
+
+  pictureChange(value: HTMLInputElement): void {
+    this.file = value.files[0];
+  }
+}
