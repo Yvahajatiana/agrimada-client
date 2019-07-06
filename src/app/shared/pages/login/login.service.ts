3cb6304c68475f47bd1e8c0ab680a90581a9103c
Stephano Romain <stephano.romain@gmail.com>
2019-07-06 21:03:46 +0100

    make dynamic oauth credential

--- src/app/shared/pages/login/login.service.ts
+++ src/app/shared/pages/login/login.service.ts
@@ -1,21 +1,44 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
-  constructor(private httpClient: HttpClient) {}
+  private credential: { client_id: string; client_secret: string; };
+  constructor(private httpClient: HttpClient) {
+    this.getCredential().subscribe((value) => {
+      this.credential = value;
+      console.log('credential', value);
+    });
+  }
 
   login(credential: Credential): Observable<PassportCredential> {
-    credential.client_id = '2';
-    credential.client_secret = 'SuKEZ0xFU3BX3JWdm2O2KeitqeM7t0iHMOIjgnif';
+    credential = {...credential, ...this.credential};
+    // credential.client_id = '2';
+    // credential.client_secret = 'SuKEZ0xFU3BX3JWdm2O2KeitqeM7t0iHMOIjgnif';
     credential.grant_type = 'password';
     return this.httpClient.post<PassportCredential>(
       `/api/oauth/token`,
       credential
     );
+  }
+
+  private getCredential() {
+    if (this.credential) {
+      return of(this.credential);
+    } else if (!this.credential && !environment.production) {
+      return this.httpClient.get<Credential>('/config/tokens');
+    } else {
+      const element = document.querySelector('html');
+      const credential = {
+        client_id: element.attributes.getNamedItem('data-clt-id').value,
+        client_secret: element.attributes.getNamedItem('data-clt-s').value
+      };
+      return of(credential);
+    }
   }
 }
 
